6d151052b4d1b782aae9612f981de0295d1f3656
mdasifnawaz545 <mdasifnawaz545@gmail.com>
2025-01-30 22:53:42 +0530

    Implement Back/Previous Button

--- src/app/home/loading.tsx
+++ src/app/home/loading.tsx
@@ -3,8 +3,8 @@
 export default function loading() {
     return (
         <div className="w-full min-h-screen flex items-center justify-center backdrop-blur-lg ">
-            <div className="animate-bounce duration-150">
-                <img src="https://res.cloudinary.com/dpqdgcipi/image/upload/v1735385814/favicon.png-removebg-preview_lecmqp.png" width={300} height={75} alt="Loading..." />
+            <div className="animate-bounce duration-150 flex items-center justify-center text-center">
+                <img className="flex items-center justify-center text-center" src="https://res.cloudinary.com/dpqdgcipi/image/upload/v1737533670/image-removebg-preview_1_gl5lis.png" width={300} height={75} alt="Loading..." />
             </div>
 
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,35 +6,42 @@ import React from 'react'
 import Button from './Button'
 import { useSession } from 'next-auth/react'
 import { User } from 'next-auth'
+import { GoArrowLeft } from "react-icons/go";
 
 const Navbar = () => {
     const session = useSession();
     const user = session?.data?.user as User;
     const homeRoute = (user) ? "/home" : "/"
     const router = useRouter();
+    const handleBack = () => {
+        router.back();
+    }
     return (
-        <nav className='bg-transparent border-mywidth border-gray-600 mx-8 mt-2 px-4 text-center text-bold text-2xl rounded-2xl flex items-center justify-between'>
+        <nav className='bg-transparent mx-8 mt-2 text-center text-bold text-2xl rounded-2xl'>
+            <div className='bg-transparent border-mywidth border-gray-600 px-4 text-center text-bold text-2xl rounded-2xl flex items-center justify-between w-full'>
+                <div>
+                    <a href={homeRoute}>
+                        <Image src="https://res.cloudinary.com/dpqdgcipi/image/upload/v1737533670/image-removebg-preview_1_gl5lis.png" alt="Recruitesy." width={150} height={20} />
+                    </a>
+                </div>
+                <div>
+                    {
 
-            <div>
-                <a href={homeRoute}>
-                    <Image src="https://res.cloudinary.com/dpqdgcipi/image/upload/v1737533670/image-removebg-preview_1_gl5lis.png" alt="Recruitesy." width={150} height={20} />
-                </a>
+                        (user) ? (
+                            <div className='border-mywidth border-gray-600 w-9 h-9 rounded-full flex items-center justify-center relative'>
+                                <img className='absolute rounded-full w-8 h-8' src={user.image as string} alt="" />
+                            </div>
+                        ) : (
+                            null
+                        )
+
+
+                    }
+                </div >
             </div>
-            <div>
-                {
-
-                    (user) ? (
-                        <div className='border-mywidth border-gray-600 w-9 h-9 rounded-full flex items-center justify-center relative'>
-                            <img className='absolute rounded-full w-8 h-8' src={user.image as string} alt="" />
-                        </div>
-                    ) : (
-                        null
-                    )
-
-
-                }
-            </div >
-
+            <div className='text-white flex items-center justify-start pt-4'>
+                <button onClick={handleBack} className='hover:shadow-md hover:shadow-blue-400 border-mywidth border-gray-600 hover:duration-200 ml-2 rounded-lg p-1'><GoArrowLeft /></button>
+            </div>
         </nav >
     )
 }
